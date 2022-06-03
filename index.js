0190a9374cb94558816a103e3ac43a65b6fb4d8c
John Sauter <John_Sauter@systemeyescomputerstore.com>
2022-06-03 16:38:30 -0400

    Write the top-level question

---
+++ index.js
@@ -0,0 +1,102 @@
+/* Employee Tracker */
+
+// Include the packages needed for this application.
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const console_table = require("console.table");
+//const generate_markdown = require("./utils/generateMarkdown.js");
+// const fs = require("fs");
+
+/* Ask the user which procedure he wishes to follow.  */
+
+function top_level_choice() {
+
+    /* Ask the user what he would like to do.  */
+    const choice_questions = [
+        {
+            type: "list",
+            name: "choice",
+            message: "Which of the following actions would you" +
+                " like to perform?",
+            choices: [
+                "view all departments", 
+                "view all roles", 
+                "view all employees",
+                "add a department",
+                "add a role",
+                "add an employee",
+                "update an employee role",
+                "exit"
+            ],
+            default: "exit"
+        }
+    ];
+    inquirer.prompt(choice_questions)
+        .then(process_choice_answer);
+}
+
+/* Process the response to the choice question.  */
+function process_choice_answer(answer) {
+    
+    /* All of the cases are asynchronous and call
+     * top_level_choice when they are done, except
+     * exit, which does not and therefore just exits.
+     */
+    switch (answer.choice) {
+        case "view all departments":
+            do_view_all_departments();
+            break;
+        case "view_all_roles":
+            do_view_all_roles();
+            break;
+        case "view_all_employees":
+            do_view_all_employees();
+            break;
+        case "add a department":
+            do_add_a_department();
+            break;
+        case "add a role":
+            do_add_a_role();
+            break;
+        case "add an employee":
+            do_add_an_employees();
+            break;
+        case "update an employee role":
+            do_update_an_employee_role();
+            break;
+        case "exit":
+            break;
+        default:
+            break;
+    }
+}
+
+/* Respond to the user's choice of procedure.  */
+function do_view_all_departments() {
+    top_level_choice();
+}
+
+function do_view_all_roles () {
+    top_level_choice();
+}
+
+function do_view_all_employees () {
+    top_level_choice();
+}
+
+function do_add_a_department () {
+    top_level_choice();
+}
+
+function do_add_a_role () {
+    top_level_choice();
+}
+
+function do_add_an_employee () {
+    top_level_choice();
+}
+
+function do_update_an_employee_role () {
+    top_level_choice();
+}
+top_level_choice();
