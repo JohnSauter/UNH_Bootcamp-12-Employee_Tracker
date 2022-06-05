826b57555953ad583e4e8ef1faca419542ab168d
John Sauter <John_Sauter@systemeyescomputerstore.com>
2022-06-04 20:20:45 -0400

    implement the simplest report

--- index.js
+++ index.js
@@ -5,18 +5,20 @@ const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const console_table = require("console.table");
 
+let connection_open = 0;
 /* Connect to the database.  */
 const db = mysql.createConnection(
     {
-      host: 'localhost',
-      // MySQL username,
-      user: 'John_',
-      // MySQL password
-      password: '',
-      database: 'employee_tracker_db'
+        host: 'localhost',
+        // MySQL username,
+        user: 'John_',
+        // MySQL password
+        password: '',
+        database: 'employee_tracker_db'
     },
     console.log(`Connected to the employee_tracker_db database.`)
 );
+connection_open = 1;
 
 /* Ask the user which procedure he wishes to follow.  
  * We come back here after each procedure.  */
@@ -31,8 +33,8 @@ function top_level_choice() {
             message: "Which of the following actions would you" +
                 " like to perform?",
             choices: [
-                "view all departments", 
-                "view all roles", 
+                "view all departments",
+                "view all roles",
                 "view all employees",
                 "add a department",
                 "add a role",
@@ -49,7 +51,7 @@ function top_level_choice() {
 
 /* Process the response to the choice question.  */
 function process_choice_answer(answer) {
-    
+
     /* All of the cases are asynchronous and call
      * top_level_choice when they are done, except
      * exit, which does not and therefore just 
@@ -72,45 +74,56 @@ function process_choice_answer(answer) {
             do_add_a_role();
             break;
         case "add an employee":
-            do_add_an_employees();
+            do_add_an_employee();
             break;
         case "update an employee role":
             do_update_an_employee_role();
             break;
         case "exit":
+            if (connection_open == 1) {
+                db.end();
+                db.destroy();
+                connection_open = 0;
+            }
             break;
         default:
             break;
     }
-    db.end();
 }
 
 /* Respond to the user's choice of procedure.  */
 function do_view_all_departments() {
+    /* select * from department;  */
+    db.query("select * from department",
+        function (err, results) {
+            if (err) { throw err; };
+            console.table(results);
+            top_level_choice();
+        }
+    )
+}
+
+function do_view_all_roles() {
     top_level_choice();
 }
 
-function do_view_all_roles () {
+function do_view_all_employees() {
     top_level_choice();
 }
 
-function do_view_all_employees () {
+function do_add_a_department() {
     top_level_choice();
 }
 
-function do_add_a_department () {
+function do_add_a_role() {
     top_level_choice();
 }
 
-function do_add_a_role () {
+function do_add_an_employee() {
     top_level_choice();
 }
 
-function do_add_an_employee () {
-    top_level_choice();
-}
-
-function do_update_an_employee_role () {
+function do_update_an_employee_role() {
     top_level_choice();
 }
 top_level_choice();
