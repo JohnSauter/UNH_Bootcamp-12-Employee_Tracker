da3fe44704ba2e12c92737fa3f5b979564c27609
John Sauter <John_Sauter@systemeyescomputerstore.com>
2022-06-06 16:59:36 -0400

    Add add employee

--- index.js
+++ index.js
@@ -261,10 +261,113 @@ function process_role_answers_2(answers) {
 }
 
 function do_add_an_employee() {
-    top_level_choice();
+    /* Ask the user for the first name, last name, role and
+     * manager of the new employee.  */
+    const employee_questions = [
+        {
+            type: "input",
+            name: "first_name",
+            message: "What is the first name of the employee?",
+            default: "none"
+        },
+        {
+            type: "input",
+            name: "last_name",
+            message: "What is the last name of the employee?",
+            default: "none"
+        },
+        {
+            type: "input",
+            name: "title",
+            message: "What is the role (title) of the employee?",
+            default: "none"
+        },
+        {
+            type: "input",
+            name: "manager_name",
+            message: "Who is the employee's manager?",
+            default: "none"
+        }
+    ];
+    inquirer.prompt(employee_questions)
+        .then(process_employee_answers_1);
+}
+
+function process_employee_answers_1(answers) {
+    const title = answers.title;
+    const safe_title = title.replace(quotation_mark, "''");
+    /* Convert title into role id
+     * and add it to answers.  */
+    const SQL_query = "select id from role where " +
+        "role.title = '" + safe_title + "';";
+    db.query(SQL_query,
+        function (err, results) {
+            if (err) { throw err; };
+            console.log(results);
+            if (results.length == 0) {
+                console.log("There is no role entitled " +
+                    title + ".");
+                top_level_choice();
+                return;
+            }
+            const role_id = results[0].id;
+            answers["role_id"] = role_id;
+            process_employee_answers_2(answers);
+        }
+    )
+}
+
+function process_employee_answers_2(answers) {
+    const manager_name = answers.manager_name;
+    const safe_manager_name = manager_name.replace(quotation_mark, "''");
+    /* Convert manager name into employee id
+     * and add it to answers.  */
+    const SQL_query = "select id from employee where " +
+        "concat(employee.first_name, space(1), employee.last_name) " +
+        " = '" + safe_manager_name + "';";
+    db.query(SQL_query,
+        function (err, results) {
+            if (err) { throw err; };
+            console.log(results);
+            if (results.length == 0) {
+                console.log("There is no manager named " +
+                    manager_name + ".");
+                top_level_choice();
+                return;
+            }
+            const manager_id = results[0].id;
+            answers["manager_id"] = manager_id;
+            process_employee_answers_3(answers);
+        }
+    )
+}
+
+function process_employee_answers_3(answers) {
+    const first_name = answers.first_name;
+    const safe_first_name = first_name.replace(quotation_mark, "''");
+    const last_name = answers.last_name;
+    const safe_last_name = last_name.replace(quotation_mark, "''");
+    const role_id = answers.role_id;
+    const manager_id = answers.manager_id;
+
+    const SQL_query = "insert into employee " +
+        "(first_name, last_name, role_id, manager_id) " +
+        " values ('" + safe_first_name + "', " +
+        "'" + safe_last_name + "', " +
+        "'" + role_id + "', " +
+        "'" + manager_id + "');";
+    db.query(SQL_query,
+        function (err, results) {
+            if (err) { throw err; };
+            console.log("Employee " + first_name +
+                " " + last_name + " added.");
+            top_level_choice();
+        }
+    )
 }
 
 function do_update_an_employee_role() {
     top_level_choice();
 }
+/* We start by asking the user what he wants to do.  */
 top_level_choice();
